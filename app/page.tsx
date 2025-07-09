cf92a30bf898ec8303985bce2e840ec87a4c9dbb
Igor <igor@asklisa.com.br>
2025-07-09 16:08:16 -0300

    projeto inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,97 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Delete, Plus, Send } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	const [_phoneNumber, _setPhoneNumberr] = React.useState("+55");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const formatPhoneNumber = (phone: string) => {
+		const digits = phone.replace(/\D/g, "");
+
+		let formatted = "+";
+		if (digits.length > 0) {
+			formatted += digits.slice(0, 2);
+		}
+		if (digits.length > 2) {
+			formatted += ` (${digits.slice(2, 4)}`;
+		}
+		if (digits.length > 4) {
+			formatted += `) ${digits.slice(4, 9)}`;
+		}
+		if (digits.length > 8) {
+			formatted += `-${digits.slice(9, 13)}`;
+		}
+
+		return formatted.trim();
+	};
+
+	return (
+		<div className="flex h-screen w-full items-center justify-center">
+			<Card className="w-full max-w-sm rounded-2xl">
+				<CardHeader>
+					<CardTitle className="text-sm">
+						Digite o numero com quem você quer conversar
+						<div className="w-full text-2xl bg-background rounded-2xl p-2 mt-4 h-20 flex items-center ">
+							{formatPhoneNumber(_phoneNumber)}
+						</div>
+					</CardTitle>
+				</CardHeader>
+				<CardContent className="flex gap-4 flex-wrap justify-center flex-row-reverse">
+					{Array.from({ length: 9 }, (_, i) => 9 - i).map((number) => (
+						<Button
+							key={number}
+							variant={"outline"}
+							className="h-20 w-24 text-2xl"
+							onClick={() => _setPhoneNumberr(_phoneNumber + number)}
+						>
+							{number}
+						</Button>
+					))}
+					<Button
+						variant={"outline"}
+						className="h-20 w-24"
+						onClick={() => _setPhoneNumberr(_phoneNumber.slice(0, -1))}
+					>
+						<Delete className="size-7" />
+					</Button>
+					<Button
+						variant={"outline"}
+						className="h-20 w-24"
+						onClick={() => _setPhoneNumberr(`${_phoneNumber}+`)}
+					>
+						<Plus className="size-7" />
+					</Button>
+
+					<Button
+						key={0}
+						variant={"outline"}
+						className="h-20 w-24 text-2xl"
+						onClick={() => _setPhoneNumberr(_phoneNumber + 0)}
+					>
+						{0}
+					</Button>
+				</CardContent>
+				<CardFooter className="flex justify-center">
+					<Button
+						variant={"default"}
+						className="h-20 w-full"
+						onClick={() => {
+							redirect(`http://wa.me/${_phoneNumber}?text=Olá`);
+						}}
+					>
+						<Send className="size-7" />
+					</Button>
+				</CardFooter>
+			</Card>
+		</div>
+	);
 }
